3a4bb0c1efeea03ff5869c30ee26c7108d68140d
Marcus Barbu <mgb.barbu@gmail.com>
2017-08-03 14:00:20 -0500

    Added client batch files

---
+++ ClientSide/README.md
@@ -0,0 +1,3 @@
+For most of the examples, port5805client.bat will access the image sent from gstreamer on the Jetson.
+
+For the dual pipeline example, both port5805client.bat and port5806client.bat are needed.
---
+++ ClientSide/port5806client.bat
@@ -0,0 +1,2 @@
+gst-launch-1.0.exe -vvv -e udpsrc port=5806 ! "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! avdec_h264 ! autovideosink
+
