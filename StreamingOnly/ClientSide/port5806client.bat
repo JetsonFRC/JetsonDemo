2115bb29e8c13086447d3ccd80bc4d690c9dd6e8
Alexander Cohen <alxcoh@gmail.com>
2017-08-18 12:24:13 -0400

    updated gstreamer commands

--- StreamingOnly/ClientSide/port5806client.bat
+++ StreamingOnly/ClientSide/port5806client.bat
@@ -1,2 +1,3 @@
+cd C:\gstreamer\1.0\x86\bin 
 gst-launch-1.0.exe -vvv -e udpsrc port=5806 ! "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! avdec_h264 ! autovideosink
-
+pause
